129af3a1d6114de30fba73043eea25e0a9759484
Kohei Yoshino <kohei.yoshino@gmail.com>
2015-02-17 00:18:12 -0500

    Fix #43 Decide the source code license

--- webroot/static/scripts/controllers/bugzfeed.js
+++ webroot/static/scripts/controllers/bugzfeed.js
@@ -3,6 +3,10 @@
  * Copyright © 2015 Kohei Yoshino. All rights reserved.
  *
  * See https://wiki.mozilla.org/BMO/ChangeNotificationSystem for the details of the API.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 BzDeck.controllers.BugzfeedClient = function BugzfeedClient () {
