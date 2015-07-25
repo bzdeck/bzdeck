5fe0363a227f2ecbc9787949c0cdc31f854cdd9b
Kohei Yoshino <kohei.yoshino@gmail.com>
2015-07-25 18:24:15 -0400

    Fix comments for #297

--- webroot/integration/bugzilla-auth-callback/index.php
+++ webroot/integration/bugzilla-auth-callback/index.php
@@ -9,8 +9,8 @@ function validate_api_key ($value) {
 }
 
 // Obtain the user credentials: The initial Bugzilla Auth Delegation implementation was using URL params to pass the
-// user name and API key to this callback page, but later updated to use the POST method for a better security practice.
-// This code supports both POST and GET for backward compatibility.
+// user name and API key to this callback page, but later updated to use the POST method for a better security practice
+// (Bug 1175643). This code supports both POST and GET for backward compatibility.
 $client_api_login = filter_var($_POST['client_api_login'] ? $_POST['client_api_login'] : $_GET['client_api_login'],
                                FILTER_VALIDATE_EMAIL, array('options' => array('default' => '')));
 $client_api_key = filter_var($_POST['client_api_key'] ? $_POST['client_api_key'] : $_GET['client_api_key'],
@@ -31,7 +31,7 @@ $client_api_key = filter_var($_POST['client_api_key'] ? $_POST['client_api_key']
 
         // window.open doesn't work on the Android WebAppRT (Bug 1183897) so this page is *not* in a sub window. In that
         // case, store the user credentials in a temporary local storage and transition to the app's landing page.
-        // Otherwise, notify the credentials to the main winow over a BroadcastChannel and close this sub window.
+        // Otherwise, notify the credentials to the main window over a BroadcastChannel and close this sub window.
         if (navigator.userAgent.includes('Android')) {
           sessionStorage.setItem('client_api_login', client_api_login);
           sessionStorage.setItem('client_api_key', client_api_key);
