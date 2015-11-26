49ae2e4ebadd3a71e31f6f528d4483a4303c09a5
Kohei Yoshino <kohei.yoshino@gmail.com>
2015-11-25 23:27:56 -0800

    Clean up BannerView

--- webroot/static/scripts/views/banner.js
+++ webroot/static/scripts/views/banner.js
@@ -12,89 +12,27 @@
  * @return {Object} view - New BannerView instance.
  */
 BzDeck.views.Banner = function BannerView (user) {
-  let mobile = this.helpers.env.device.mobile,
-      $$tablist = this.$$tablist = new this.widgets.TabList(document.querySelector('#main-tablist')),
-      $root = document.documentElement, // <html>
-      $sidebar = document.querySelector('#sidebar');
-
+  this.$$tablist = new this.widgets.TabList(document.querySelector('#main-tablist'));
+  this.$$tablist.bind('Selected', event => this.on_tab_selected(event.detail));
   this.tab_path_map = new Map([['tab-home', '/home/inbox']]);
 
-  $$tablist.bind('Selected', event => {
-    let path = this.tab_path_map.get(event.detail.items[0].id);
-
-    $root.setAttribute('data-current-tab', path.match(/^\/(\w+)/)[1]);
-
-    // Do not apply transition to the previous tabpanel. This is a tricky part!
-    {
-      let prev_tabpanel_id;
-
-      if (!path.startsWith('/home/') && history.state && event.detail.oldval.length &&
-          history.state.previous === this.tab_path_map.get(event.detail.oldval[0].id)) {
-        prev_tabpanel_id = event.detail.oldval[0].id.replace(/^tab-/, 'tabpanel-');
-      }
-
-      for (let $tabpanel of document.querySelectorAll('#main-tabpanels > [role="tabpanel"]')) {
-        if ($tabpanel.id === prev_tabpanel_id) {
-          $tabpanel.classList.add('fixed');
-        } else {
-          $tabpanel.classList.remove('fixed');
-        }
-      }
-    }
-
-    this.trigger(':TabSelected', { path });
-  });
-
   // Make the logo clickable
-  document.querySelector('[role="banner"] h1').addEventListener('mousedown', event => this.trigger(':LogoClicked'));
+  this.$logo = document.querySelector('[role="banner"] h1');
+  this.$logo.addEventListener('mousedown', event => this.trigger(':LogoClicked'));
 
-  new this.widgets.MenuBar(document.querySelector('#main-menu'));
+  this.$$menu = new this.widgets.MenuBar(document.querySelector('#main-menu'));
+  this.$app_menu = document.querySelector('#main-menu--app-menu');
 
-  let $app_menu = document.querySelector('#main-menu--app-menu');
-
-  $app_menu.addEventListener('MenuItemSelected', event => {
+  this.$app_menu.addEventListener('MenuItemSelected', event => {
     this.trigger(':AppMenuItemSelected', { command: event.detail.command });
   });
 
-  // Switch to the mobile layout
-  if (mobile) {
-    document.querySelector('#sidebar-account').appendChild(document.querySelector('#main-menu--app--account'));
-    document.querySelector('#sidebar-menu').appendChild($app_menu);
-    document.querySelector('#tabpanel-home [role="toolbar"]')
-            .appendChild(document.querySelector('#quicksearch'));
-    document.querySelector('#tabpanel-home [role="toolbar"]')
-            .appendChild(document.querySelector('#toolbar-buttons'));
-
-    $app_menu.addEventListener('MenuClosed', event => {
-      // Keep the menu open
-      $app_menu.removeAttribute('aria-expanded');
-      // Hide the sidebar
-      $root.setAttribute('data-sidebar-hidden', 'true');
-      $sidebar.setAttribute('aria-hidden', 'true');
-    });
+  if (this.helpers.env.device.mobile) {
+    this.apply_mobile_layout();
   }
 
-  $app_menu.setAttribute('aria-expanded', mobile);
-
   if (this.helpers.app.fullscreen_enabled) {
-    {
-      let $menuitem = document.querySelector('#main-menu--app--fullscreen');
-
-      let toggle_fullScreen = () => {
-        document.mozFullScreenElement ? document.mozCancelFullScreen() : document.body.mozRequestFullScreen();
-      };
-
-      $menuitem.removeAttribute('aria-hidden');
-
-      // A workaround for Bug 779324
-      $menuitem.addEventListener('mousedown', event => toggle_fullScreen());
-      this.helpers.kbd.assign($menuitem, { Enter: event => toggle_fullScreen() });
-
-      window.addEventListener('mozfullscreenchange', event => {
-        $menuitem.querySelector('label').textContent = document.mozFullScreenElement ? 'Exit Full Screen'
-                                                                                     : 'Enter Full Screen'; // l10n
-      });
-    }
+    this.activate_fullscreen_menuitem();
   }
 
   // Show the Quit menu item if the app runs on WebAppRT
@@ -102,33 +40,94 @@ BzDeck.views.Banner = function BannerView (user) {
     document.querySelector('#main-menu--app--quit').removeAttribute('aria-hidden');
   }
 
-  // Account label & avatar
-  {
-    let label = `<strong>${user.name}</strong><br>${user.email}`,
-        $menu_label = document.querySelector('#main-menu--app label'),
-        $account_label = document.querySelector('#main-menu--app--account label');
-
-    $account_label.innerHTML = label;
-    $account_label.style['background-image'] = $menu_label.style['background-image'] = `url(${user.image})`;
-
-    this.on('C:GravatarProfileFound', data => {
-      document.querySelector('#sidebar-account').style['background-image'] = data.style['background-image'];
-    });
-  }
-
-  // App install
-  {
-    let $menuitem = document.querySelector('#main-menu--app--install');
-
-    this.on('AppInstalled', () => $menuitem.setAttribute('aria-disabled', 'true'), true)
-    this.helpers.app.can_install().then(() => $menuitem.removeAttribute('aria-hidden')).catch(error => {});
-  }
-
+  this.setup_account_label(user);
+  this.setup_app_install_menuitem();
   this.setup_throbber();
 };
 
 BzDeck.views.Banner.prototype = Object.create(BzDeck.views.Base.prototype);
 BzDeck.views.Banner.prototype.constructor = BzDeck.views.Banner;
+
+/**
+ * Switch to the mobile layout.
+ *
+ * @argument {undefined}
+ * @return {undefined}
+ */
+BzDeck.views.Banner.prototype.apply_mobile_layout = function () {
+  document.querySelector('#sidebar-account').appendChild(document.querySelector('#main-menu--app--account'));
+  document.querySelector('#sidebar-menu').appendChild(this.$app_menu);
+  document.querySelector('#tabpanel-home [role="toolbar"]').appendChild(document.querySelector('#quicksearch'));
+  document.querySelector('#tabpanel-home [role="toolbar"]').appendChild(document.querySelector('#toolbar-buttons'));
+  this.$app_menu.setAttribute('aria-expanded', 'true');
+
+  this.$app_menu.addEventListener('MenuClosed', event => {
+    // Keep the menu open
+    this.$app_menu.removeAttribute('aria-expanded');
+    // Hide the sidebar
+    document.documentElement.setAttribute('data-sidebar-hidden', 'true');
+    document.querySelector('#sidebar').setAttribute('aria-hidden', 'true');
+  });
+};
+
+/**
+ * Activate the fullscreen menu item.
+ *
+ * @argument {undefined}
+ * @return {undefined}
+ */
+BzDeck.views.Banner.prototype.activate_fullscreen_menuitem = function () {
+  let $menuitem = document.querySelector('#main-menu--app--fullscreen'),
+      $label = $menuitem.querySelector('label');
+
+  let toggle = () => {
+    document.mozFullScreenElement ? document.mozCancelFullScreen() : document.body.mozRequestFullScreen();
+  };
+
+  $menuitem.removeAttribute('aria-hidden');
+
+  // A workaround for Bug 779324
+  $menuitem.addEventListener('mousedown', event => toggle());
+  this.helpers.kbd.assign($menuitem, { Enter: event => toggle() });
+
+  window.addEventListener('mozfullscreenchange', event => {
+    $label.textContent = document.mozFullScreenElement ? 'Exit Full Screen' : 'Enter Full Screen'; // l10n
+  });
+};
+
+/**
+ * Set up the account label & avatar.
+ *
+ * @argument {Object} user - User info.
+ * @argument {String} user.name - User's full name.
+ * @argument {String} user.email - User's email address.
+ * @argument {String} user.image - User's avatar image URL.
+ * @return {undefined}
+ */
+BzDeck.views.Banner.prototype.setup_account_label = function (user) {
+  let $menu_label = document.querySelector('#main-menu--app label'),
+      $account_label = document.querySelector('#main-menu--app--account label');
+
+  $account_label.innerHTML = `<strong>${user.name}</strong><br>${user.email}`;
+  $account_label.style['background-image'] = $menu_label.style['background-image'] = `url(${user.image})`;
+
+  this.on('C:GravatarProfileFound', data => {
+    document.querySelector('#sidebar-account').style['background-image'] = data.style['background-image'];
+  });
+};
+
+/**
+ * Set up the app install menu item.
+ *
+ * @argument {undefined}
+ * @return {undefined}
+ */
+BzDeck.views.Banner.prototype.setup_app_install_menuitem = function () {
+  let $menuitem = document.querySelector('#main-menu--app--install');
+
+  this.on('AppInstalled', () => $menuitem.setAttribute('aria-disabled', 'true'), true);
+  this.helpers.app.can_install().then(() => $menuitem.removeAttribute('aria-hidden')).catch(error => {});
+};
 
 /**
  * Set up the activity indicator or "throbber" displayed while loading bugs.
@@ -164,7 +163,6 @@ BzDeck.views.Banner.prototype.open_tab = function (options, controller) {
       page_constructor = options.page_constructor,
       page_constructor_args = options.page_constructor_args || [],
       pages = BzDeck.views.pages[`${page_category}_list`],
-      $$tablist = BzDeck.views.banner.$$tablist,
       tab_id = options.page_category + (page_id ? '-' + page_id : ''),
       tab_label = options.tab_label,
       tab_desc = options.tab_desc || tab_label,
@@ -179,10 +177,10 @@ BzDeck.views.Banner.prototype.open_tab = function (options, controller) {
   // Reuse a tabpanel if possible
   if ($tabpanel) {
     page = pages.get(page_id || 'default');
-    $tab = $tab || $$tablist.add_tab(tab_id, tab_label, tab_desc, $tabpanel, tab_position);
+    $tab = $tab || this.$$tablist.add_tab(tab_id, tab_label, tab_desc, $tabpanel, tab_position);
   } else {
     $tabpanel = this.get_template(`tabpanel-${page_category}-template`, page_id);
-    $tab = $$tablist.add_tab(tab_id, tab_label, tab_desc, $tabpanel, tab_position);
+    $tab = this.$$tablist.add_tab(tab_id, tab_label, tab_desc, $tabpanel, tab_position);
     page = controller.view = new page_constructor(...page_constructor_args);
     page.controller = controller;
     pages.set(page_id || 'default', page);
@@ -191,12 +189,44 @@ BzDeck.views.Banner.prototype.open_tab = function (options, controller) {
     this.add_back_button($tabpanel);
   }
 
-  $$tablist.view.selected = $$tablist.view.$focused = $tab
+  this.$$tablist.view.selected = this.$$tablist.view.$focused = $tab
   $tabpanel.focus();
 
   BzDeck.views.global.update_window_title($tab);
   BzDeck.views.pages[page_category] = page;
   this.tab_path_map.set($tab.id, location.pathname + location.search);
+};
+
+/**
+ * Called whenever a global tab is selected.
+ *
+ * @argument {Object} detail - Event detail.
+ * @argument {Array.<HTMLElement>} detail.items - Newly selected nodes.
+ * @argument {Array.<HTMLElement>} detail.oldval - Previously selected nodes.
+ * @return {undefined}
+ */
+BzDeck.views.Banner.prototype.on_tab_selected = function (detail) {
+  let { items, oldval } = detail,
+      path = this.tab_path_map.get(items[0].id),
+      prev_tabpanel_id;
+
+  document.documentElement.setAttribute('data-current-tab', path.match(/^\/(\w+)/)[1]);
+
+  // Do not apply transition to the previous tabpanel. This is a tricky part!
+  if (!path.startsWith('/home/') && history.state && oldval.length &&
+      history.state.previous === this.tab_path_map.get(oldval[0].id)) {
+    prev_tabpanel_id = oldval[0].id.replace(/^tab-/, 'tabpanel-');
+  }
+
+  for (let $tabpanel of document.querySelectorAll('#main-tabpanels > [role="tabpanel"]')) {
+    if ($tabpanel.id === prev_tabpanel_id) {
+      $tabpanel.classList.add('fixed');
+    } else {
+      $tabpanel.classList.remove('fixed');
+    }
+  }
+
+  this.trigger(':TabSelected', { path });
 };
 
 /**
