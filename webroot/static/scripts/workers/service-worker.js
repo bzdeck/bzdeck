d7b51902b2da1d3e6639702e42915dbc22484a52
Kohei Yoshino <kohei.yoshino@gmail.com>
2016-02-20 15:46:31 -0500

    Bump the version for release

--- webroot/static/scripts/workers/service-worker.js
+++ webroot/static/scripts/workers/service-worker.js
@@ -5,7 +5,7 @@
 'use strict';
 
 // Cache version: this has to be updated whenever a file is modified
-const version = '2016-02-20-11-19';
+const version = '2016-02-20-15-46';
 
 // Files need to be cached
 const files = [
